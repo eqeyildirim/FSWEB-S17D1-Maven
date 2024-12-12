eff9664d6b72e79cf853b41cd75243451f97e1d6
eqeyildirim <eqeyildirims@gmail.com>
2024-12-11 23:18:42 -0300

    done

--- src/main/java/com/workintech/fswebs17d1/FswebS17d1Application.java
+++ src/main/java/com/workintech/fswebs17d1/FswebS17d1Application.java
@@ -1,13 +1,23 @@
 package com.workintech.fswebs17d1;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FswebS17d1Application {
+	@Value("${course.name}")
+	private String courseName;
 
+	@Value("${project.developer.fullname}")
+	private String developerName;
 	public static void main(String[] args) {
 		SpringApplication.run(FswebS17d1Application.class, args);
 	}
-
+	@PostConstruct
+	public void printProperties() {
+		System.out.println("Course Name: " + courseName);
+		System.out.println("Developer Name: " + developerName);
+	}
 }
